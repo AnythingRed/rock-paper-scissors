21b5d35bc08a3cb59475f2e04f00d00af90b4397
AnythingRed <130374881+AnythingRed@users.noreply.github.com>
2023-04-29 03:59:49 +0200

    Fix existing  functions

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-/* Begin with a function getComputerChoice that will randomly return either rock paper or scissors */
-/* choose a random number between 0 and 2 and assign a string to each */
+/* global variables */
 let computerChoice;
+let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
+/* computer chooses fighter (no problems) */
 function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * 3)
   if (randomNum === 0) {
@@ -14,33 +17,57 @@ function getComputerChoice() {
     computerChoice = 'Unexpected Error in getComputerChoice function';
   }
 }
-/* make playerSelection case insensitive */
 
-let playerSelection = prompt('Choose between rock, paper or scissors').toLowerCase();
-console.log(playerSelection);
-/* write a funcction that plays a single round of rock paper scissors. take two parameters playerSelection and computerSelection. return a string that declares the winner of the round like so: You lose! Paper beats Rock */
+/* user chooses fighter (no problems) */
+function getPlayerSelection() {
+  playerSelection = prompt('Choose between rock, paper or scissors')
+  
+  if (!playerSelection) {
+    console.log(`You didn't enter anything. Refresh the page if you wanna try again.`)
+  } else if (playerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+      return playerSelection;
+    } else {
+      console.log(`You have to choose between rock, paper and scissors! Try again.`)
+      getPlayerSelection();
+    }
+  }
+}
 
-function round() {
+/* function that plays a single round of rock paper scissors. takes two parameters playerSelection and computerSelection. returns a string that declares the winner of the round (no problems) */
+function playRound() {
+  getComputerChoice();
+  console.log(computerChoice);
+  getPlayerSelection();
+  console.log(playerSelection);
   switch(true) {
     case computerChoice === playerSelection:
       return `It's a draw! You both chose ${computerChoice}.`;
       break;
     case computerChoice === 'rock' && playerSelection === 'paper':
+      ++playerScore;
       return `You win! Paper beats rock.`;
       break;
     case computerChoice === 'rock' && playerSelection === 'scissors':
+      ++computerScore;
       return `You lose! Rock beats scissors`;
       break;
     case computerChoice === 'paper' && playerSelection === 'rock':
+      ++computerScore;
       return `You lose! Paper beats rock.`;
       break;
     case computerChoice === 'paper' && playerSelection === 'scissors':
+      ++playerScore;
+      console.log(playerScore);
       return `You win! Scissors beat paper.`;
       break;
     case computerChoice === 'scissors' && playerSelection === 'rock':
+      ++playerScore;
       return `You win! Rock beats scissors`;
       break;
     case computerChoice === 'scissors' && playerSelection === 'paper':
+      ++computerScore;
       return `You lose! Scissors beat paper.`;
       break;
     default:
@@ -48,6 +75,25 @@ function round() {
       break;
   }
 }
-getComputerChoice();
-console.log(computerChoice);
-console.log(round());
+
+/* write a new function called game. use the previous function to play a 5 round game that keeps score and reports a winner or loser at the end. */
+/* use a loop to do 5 rounds or call playround function 5 times */
+
+
+function game() {
+let i = 0;
+
+  while (i < 5) {
+    playRound();
+    console.log(playerScore);
+    i++
+  }
+}
+
+playRound();
+console.log(playerScore);
+console.log(computerScore);
+/* var score and update it every turn */
+
+/* report winner after every round */
+
